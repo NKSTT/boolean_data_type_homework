9831617f935de26d87d8299efbde08253571cefb
NKSTT <asultamuratov110@gmail.com>
2024-10-27 21:55:12 +0800

    bool misol 10

--- bool10.py
+++ bool10.py
@@ -1,10 +1,7 @@
+from math import sqrt
 def main(a):
-    """
-    Check that the number "a" is a perfect square.
-    Args:
-        a: int
-    Returns:
-        bool
-    """
-    # Write your code here
-    return 
+    return a >= 0 and sqrt(a)**2 == a
+print (main(9))
+print (main(15))
+print (main(121))
+print (main(111))
